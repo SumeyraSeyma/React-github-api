15dd78d925948cf90a7375bd9d045a65ce11b765
SumeyraSeyma <114585202+SumeyraSeyma@users.noreply.github.com>
2024-09-02 13:42:17 +0300

    .

--- github-api/src/App.js
+++ github-api/src/App.js
@@ -4,11 +4,21 @@ import List from './components/List/List';
 
 function App() {
   const [data, setData] = useState([]);
+  const [user, setUser] = useState(null);
+  
+  function debounce(func, wait) {
+    let timeout;
+  
+    return function(...args) {
+      const context = this;
+      clearTimeout(timeout);
+      timeout = setTimeout(() => func.apply(context, args), wait);
+    };
+  }
   
 
-  useEffect(() => {
     const getData = () => {
-      fetch('https://api.github.com/users')
+      return fetch(`https://api.github.com/users/${user}`) 
         .then((response) => {
           if (!response.ok) {
             throw new Error('Network response was not ok');
@@ -18,11 +28,20 @@ function App() {
         .then(data => setData(data))
         .catch((error) => console.error('Error fetching data:', error));
   }
-  getData();
-}, []);
+
+  const debouncedGetData = debounce(getData, 5000);
+
+  useEffect(() => {
+    if (user) {
+      debouncedGetData();
+    }
+  }, [user]);
+
 
   return (
     <div className="App">
+      <input type="text" placeholder="Search for User" onChange={(e) => setUser(e.target.value)} className="input_search" />
+      <button onClick={getData} className="search_button">Search Github</button>
       <List data={data} />
     </div>
   );
--- github-api/src/components/List/List.js
+++ github-api/src/components/List/List.js
@@ -1,15 +1,31 @@
 import React from 'react'
 
 function List({ data }) {
-
+    if ( !data ) {
+        return <div> No data </div>
+    } else if (!Array.isArray(data)) {
+        <div> Data is not an array </div>
+    }else if (data.login === undefined) {
+        return <div> No data </div>
+    }
 
   return (
     <div>
         <ul>
-        {data && data.map((item, index) => (
-          <li key={index}>{item.login}</li>
-        ))}
-        </ul>
+        <li>Kullanıcı Adı: {data.login}</li>
+        <li>ID: {data.id}</li>
+        <li>Profile URL: <a href={data.html_url}>{data.html_url}</a></li>
+        <li>Followers: {data.followers}</li>
+        <li>Following: {data.following}</li>
+        <li>Public Repos: {data.public_repos}</li>
+        <li>Public Gists: {data.public_gists}</li>
+        <li>Location: {data.location}</li>
+        <li>Company: {data.company}</li>
+        <li>Email: {data.email}</li>
+        <li>Bio: {data.bio}</li>
+        <li>Node ID: {data.node_id}</li>
+        <li>Avatar URL: <img src={data.avatar_url} alt={data.login} /></li>
+      </ul>
     </div>
   )
 }
