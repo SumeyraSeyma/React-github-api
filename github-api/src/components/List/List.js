12cd716fa8ad32babd3d484a3f3a7312925df4e0
SumeyraSeyma <114585202+SumeyraSeyma@users.noreply.github.com>
2024-09-03 15:50:58 +0300

    .

--- github-api/src/components/List/List.js
+++ github-api/src/components/List/List.js
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMapMarkerAlt, faUsers, faUserPlus } from '@fortawesome/free-solid-svg-icons';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
 
 
 
@@ -13,6 +14,84 @@ function List({ data}) {
     }else if (data.login === undefined) {
         return <div> No user data </div>
     }
+
+    const langColors = {
+      JavaScript: '#f1e05a',
+      Python: '#3572A5',
+      Java: '#b07219',
+      C: '#555555',
+      'C++': '#f34b7d',
+      'C#': '#178600',
+      PHP: '#4F5D95',
+      Ruby: '#701516',
+      Go: '#00ADD8',
+      TypeScript: '#2b7489',
+      HTML: '#e34c26',
+      CSS: '#563d7c',
+      Shell: '#89e051',
+      Swift: '#ffac45',
+      Kotlin: '#A97BFF',
+      Rust: '#dea584',
+      R: '#198CE7',
+      Dart: '#00B4AB',
+      ObjectiveC: '#438eff',
+      Perl: '#0298c3',
+      Scala: '#c22d40',
+      Haskell: '#5e5086',
+      Lua: '#000080',
+      Elixir: '#6e4a7e',
+      Clojure: '#db5855',
+      Groovy: '#e69f56',
+      CoffeeScript: '#244776',
+      TeX: '#3D6117',
+      VimL: '#199f4b',
+      Smalltalk: '#596706',
+      Matlab: '#e16737',
+      Verilog: '#b2b7f8',
+      VHDL: '#adb2cb',
+      Elm: '#60B5CC',
+      Julia: '#a270ba',
+      Crystal: '#000100',
+      Forth: '#341708',
+      Scheme: '#1e4aec',
+      Solidity: '#AA6746',
+      Groff: '#ecdebe',
+      PowerShell: '#012456',
+      Pascal: '#E3F171',
+      Fortran: '#4d41b1',
+      Haxe: '#df7900',
+      JupyterNotebook: '#DA5B0B',
+      Turing: '#cf142b',
+      Vala: '#358a5b',
+      Nim: '#37775b',
+      GAMS: '#f49a22',
+      Dockerfile: '#384d54',
+      Makefile: '#427819',
+      QML: '#44a51c',
+      Less: '#1d365d',
+      Sass: '#a53b70',
+      SCSS: '#c6538c',
+      Stylus: '#ff6347',
+      YAML: '#cb171e',
+      JSON: '#292929',
+      XML: '#0060ac',
+      Batchfile: '#C1F12E',
+      Ada: '#02f88c',
+      OCaml: '#3be133',
+      ReasonML: '#ff5847',
+      FSharp: '#b845fc',
+      CommonLisp: '#3fb68b',
+      EmacsLisp: '#c065db',
+      Assembly: '#6E4C13',
+      Nix: '#7e7eff',
+      Zig: '#ec915c',
+      CMake: '#64c5b1',
+      Meson: '#007800',
+      WebAssembly: '#04133b',
+      D: '#ba595e',
+      Mathematica: '#dd1100',
+      Idris: '#b30000'
+    };
 
 
   return (
@@ -59,10 +138,14 @@ function List({ data}) {
                 data.repos && data.repos.map((repo) => (
                     <li key={repo.id} className='hover:list-disc '>
                     <a className='hover:underline hover:decoration-emerald-500' href={repo.html_url}>{repo.name}</a>
+                    <p className=''>{repo.description}</p>
                     <p>
+                      
                     {
                       repo.language && (
-                        <span className='ml-2'>{repo.language}</span>
+                        <span className='ml-2'> <FontAwesomeIcon icon={faCircle} style={{color: langColors[repo.language] || '#0000' , fontSize: '0.7rem'
+                        }}/>{repo.language}</span>
+                        
                       )
                     }
                     </p>
